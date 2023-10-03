769d91ef012c3361d9e32a89385fb3435507f1d7
Woldemar Gorn <wg84@gmx.de>
2023-10-03 10:28:11 +0200

    add new components remove unused files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Game</h1>
+  <button @click="startPlaing" :disabled="isPlaing">play</button>
+  <TargetBlock v-if="isPlaing" :delay="delay" @end="endGame" />
+  <Result v-if="isShowScore" :score="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TargetBlock from './components/TargetBlock.vue'
+import Result from './components/Result.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { TargetBlock, Result },
+  data() {
+    return {
+      isPlaing: false,
+      isShowScore: false,
+      delay: null,
+      score: 0
+    }
+  },
+  methods: {
+    startPlaing() {
+      this.isPlaing = !this.isPlaing
+      this.delay = 2000 + Math.random() * 5000
+      this.isShowScore = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaing = false
+      this.isShowScore = true
+    }
   }
 }
 </script>
@@ -22,5 +44,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  background: #296e10;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+    <p>time : {{ score }} ms</p>
+    <p class="rank">Your rank : {{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Super fast'
+        } else if (this.score < 400) {
+            this.rank = 'Ok fast'
+        } else {
+            this.rank = 'Train more'
+        }
+    }
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.5em;
+    color: rgb(225, 38, 28);
+    font-weight: bold;
+}
+</style>
